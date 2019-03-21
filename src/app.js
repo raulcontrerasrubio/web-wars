2e3a51a3cc061f820f46ba4da32234dfeae432b6
Raúl Contreras <raulc98@gmail.com>
2019-03-21 09:41:39 +0100

    Pacman GUI Example

--- src/app.js
+++ src/app.js
@@ -1,22 +1,39 @@
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
 
-window.onload = function(){
-  canvas.width = 640;
-  canvas.height = 320;
-  
+// Valores iniciales
+// gui params
+var myAngle = 30;
+var myColor = '#eeee00';
 
-  requestAnimationFrame(gameLoop);
+var gui;
+
+function setup() {
+
+  createCanvas(windowWidth, windowHeight);
+
+  // Create the GUI
+  // min, max, ?
+  sliderRange(0, 180, 1);
+  gui = createGui('p5.gui');
+  gui.addGlobals('myColor', 'myAngle');
+
+  // Only call draw when then gui is changed
+  noLoop();
+
 }
 
-function gameLoop(){
-  drawEverything();
 
-  requestAnimationFrame(gameLoop);
+function draw() {
+
+  // this is a piece of cake
+  background(0);
+  fill(myColor);
+  angleMode(DEGREES);
+  arc(width/2, height/2, 100, 100, myAngle/2, 360 - myAngle/2, PIE);
+
 }
 
-function drawEverything(){
-  // Backrgound
-  ctx.fillStyle = 'grey';
-  ctx.fillRect(0, 0, canvas.clientWidth, canvas.height);
+
+// dynamically adjust the canvas to the window
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
