8851f975a71336c9640ad4b6b66533610bb17c07
Raúl Contreras <raulc98@gmail.com>
2019-03-25 09:04:00 +0100

    Now the panels readapts to the position of the canvas automatically

--- src/app.js
+++ src/app.js
@@ -7,31 +7,34 @@ class WebWars{
       settings: {
         width: 180,
         height: 120
-      },
-      position: {
-        left: QuickSettings.create(this.root.left, this.root.top, 'LeftPanel')
-          .setHeight(this.canvas.height)
-          .setWidth(this.panels.settings.width),
-
-        right: QuickSettings.create(this.root.right - panelWidth, this.root.top, 'RightPanel')
-          .setHeight(this.canvas.height)
-          .setWidth(this.panels.settings.width),
-
-        bottom: QuickSettings.create(this.root.left, this.root.top + this.canvas.height - panelHeight, 'BottomPanel')
-          .setHeight(this.panels.settings.height)
-          .setWidth(this.canvas.width),
-
-        top: QuickSettings.create(this.root.left, this.root.top, 'TopPanel')
-          .setHeight(this.panels.settings.height)
-          .setWidth(this.canvas.width),
-      },
+      }
     };
 
     this.canvas.width = 640;
     this.canvas.height = 320;
+
+    this.panels.position = {
+      left: QuickSettings.create(this.root.left, this.root.top, 'LeftPanel')
+        .setHeight(this.canvas.height)
+        .setWidth(this.panels.settings.width),
+
+      right: QuickSettings.create(this.root.left + this.canvas.width - this.panels.settings.width, this.root.top, 'RightPanel')
+        .setHeight(this.canvas.height)
+        .setWidth(this.panels.settings.width),
+
+      bottom: QuickSettings.create(this.root.left, this.root.top + this.canvas.height - this.panels.settings.height, 'BottomPanel')
+        .setHeight(this.panels.settings.height)
+        .setWidth(this.canvas.width),
+
+      top: QuickSettings.create(this.root.left, this.root.top, 'TopPanel')
+        .setHeight(this.panels.settings.height)
+        .setWidth(this.canvas.width),
+    }
+
   }
 
   gameLoop(){
+    
     requestAnimationFrame(this.gameLoop);
   }
 
@@ -42,6 +45,7 @@ class WebWars{
 
 window.onload = function(){
   let Game = new WebWars();
-  Game.canvas
+  window.scroll = Game.reOffset;
+  window.onresize = Game.reOffset;
   console.log(Game);
 }
